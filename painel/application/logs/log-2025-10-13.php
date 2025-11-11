aeb6e77e257cc259b615763cb70884aa96f8e618
gorjessbbyx3 <jhoutz@hawaii.edu>
2025-11-11 06:57:49 +0000

    commit

---
+++ painel/application/logs/log-2025-10-13.php
@@ -0,0 +1,46 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2025-10-13 05:21:07 --> UTF-8 Support Enabled
+ERROR - 2025-10-13 05:21:07 --> Severity: 8192 --> Creation of dynamic property CI_URI::$config is deprecated C:\xampp\htdocs\donald bet\painel\system\core\URI.php 102
+ERROR - 2025-10-13 05:21:07 --> Severity: 8192 --> Creation of dynamic property CI_Router::$uri is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Router.php 128
+DEBUG - 2025-10-13 05:21:07 --> No URI present. Default controller set.
+DEBUG - 2025-10-13 05:21:07 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2025-10-13 05:21:07 --> Severity: 8192 --> Creation of dynamic property Welcome::$benchmark is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:07 --> Severity: 8192 --> Creation of dynamic property Welcome::$hooks is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:07 --> Severity: 8192 --> Creation of dynamic property Welcome::$config is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:07 --> Severity: 8192 --> Creation of dynamic property Welcome::$log is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:07 --> Severity: 8192 --> Creation of dynamic property Welcome::$utf8 is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:07 --> Severity: 8192 --> Creation of dynamic property Welcome::$uri is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:07 --> Severity: 8192 --> Creation of dynamic property Welcome::$exceptions is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:07 --> Severity: 8192 --> Creation of dynamic property Welcome::$router is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:07 --> Severity: 8192 --> Creation of dynamic property Welcome::$output is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:07 --> Severity: 8192 --> Creation of dynamic property Welcome::$security is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:07 --> Severity: 8192 --> Creation of dynamic property Welcome::$input is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:07 --> Severity: 8192 --> Creation of dynamic property Welcome::$lang is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:07 --> Severity: 8192 --> Creation of dynamic property Welcome::$db is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Loader.php 397
+ERROR - 2025-10-13 05:21:07 --> Severity: 8192 --> Creation of dynamic property CI_DB_mysqli_driver::$failover is deprecated C:\xampp\htdocs\donald bet\painel\system\database\DB_driver.php 372
+ERROR - 2025-10-13 05:21:07 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\donald bet\painel\system\database\drivers\mysqli\mysqli_driver.php 211
+ERROR - 2025-10-13 05:21:07 --> Unable to connect to the database
+ERROR - 2025-10-13 05:21:07 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp\htdocs\donald bet\painel\system\core\Exceptions.php:272) C:\xampp\htdocs\donald bet\painel\system\core\Common.php 571
+DEBUG - 2025-10-13 05:21:35 --> UTF-8 Support Enabled
+ERROR - 2025-10-13 05:21:35 --> Severity: 8192 --> Creation of dynamic property CI_URI::$config is deprecated C:\xampp\htdocs\donald bet\painel\system\core\URI.php 102
+ERROR - 2025-10-13 05:21:35 --> Severity: 8192 --> Creation of dynamic property CI_Router::$uri is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Router.php 128
+DEBUG - 2025-10-13 05:21:35 --> No URI present. Default controller set.
+DEBUG - 2025-10-13 05:21:35 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2025-10-13 05:21:35 --> Severity: 8192 --> Creation of dynamic property Welcome::$benchmark is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:35 --> Severity: 8192 --> Creation of dynamic property Welcome::$hooks is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:35 --> Severity: 8192 --> Creation of dynamic property Welcome::$config is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:35 --> Severity: 8192 --> Creation of dynamic property Welcome::$log is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:35 --> Severity: 8192 --> Creation of dynamic property Welcome::$utf8 is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:35 --> Severity: 8192 --> Creation of dynamic property Welcome::$uri is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:35 --> Severity: 8192 --> Creation of dynamic property Welcome::$exceptions is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:35 --> Severity: 8192 --> Creation of dynamic property Welcome::$router is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:35 --> Severity: 8192 --> Creation of dynamic property Welcome::$output is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:35 --> Severity: 8192 --> Creation of dynamic property Welcome::$security is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:35 --> Severity: 8192 --> Creation of dynamic property Welcome::$input is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:35 --> Severity: 8192 --> Creation of dynamic property Welcome::$lang is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Controller.php 83
+ERROR - 2025-10-13 05:21:35 --> Severity: 8192 --> Creation of dynamic property Welcome::$db is deprecated C:\xampp\htdocs\donald bet\painel\system\core\Loader.php 397
+ERROR - 2025-10-13 05:21:35 --> Severity: 8192 --> Creation of dynamic property CI_DB_mysqli_driver::$failover is deprecated C:\xampp\htdocs\donald bet\painel\system\database\DB_driver.php 372
+ERROR - 2025-10-13 05:21:35 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\donald bet\painel\system\database\drivers\mysqli\mysqli_driver.php 211
+ERROR - 2025-10-13 05:21:35 --> Unable to connect to the database
+ERROR - 2025-10-13 05:21:35 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp\htdocs\donald bet\painel\system\core\Exceptions.php:272) C:\xampp\htdocs\donald bet\painel\system\core\Common.php 571
